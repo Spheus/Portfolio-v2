49a54c438abd3d38ec3a502d119d11c24b4cdae6
Samuel Mejia <smejia@earnest.com>
2023-06-13 11:19:15 -0500

    add safelist values

--- src/app/components/RainbowText/index.tsx
+++ src/app/components/RainbowText/index.tsx
@@ -1,29 +1,30 @@
-
-const defaultColors = [
-  "text-red-400",
-  "text-orange-400",
-  "text-yellow-400",
-  "text-lime-400",
-  "text-green-400",
-  "text-teal-400",
-  "text-cyan-400",
-  "text-sky-400",
-  "text-blue-400",
-  "text-indigo-400",
-  "text-violet-400",
-];
-function RainbowText({ text = "", colors = defaultColors }) {
+function RainbowText({ text = "" }) {
   const textArray = text.split("");
-
+  const defaultColors = [
+    "group-hover/korok:text-red-400 delay-[]",
+    "group-hover/korok:text-orange-400",
+    "group-hover/korok:text-yellow-400",
+    "group-hover/korok:text-lime-400",
+    "group-hover/korok:text-green-400",
+    "group-hover/korok:text-teal-400",
+    "group-hover/korok:text-cyan-400",
+    "group-hover/korok:text-sky-400",
+    "group-hover/korok:text-blue-400",
+    "group-hover/korok:text-indigo-400",
+    "group-hover/korok:text-violet-400",
+  ];
+  defaultColors.forEach(
+    (val, i) => val + `delay-[${(i % defaultColors.length) * 25 + 50}ms]`
+  );
   return (
     <span className="group/korok inline-flex   lg:font-medium lg:text-slate-200">
       {textArray.map((val, i) => (
         <span
           key={val + i}
-          className={`whitespace-pre group-hover/korok:${
-            colors[i % colors.length]
+          className={`whitespace-pre ${
+            defaultColors[i % defaultColors.length]
           } transition duration-75 group-hover/korok:-translate-y-px delay-[${
-            (i % colors.length) * 25 + 50
+            (i % defaultColors.length) * 25 + 50
           }ms]`}
           aria-hidden="true"
         >
