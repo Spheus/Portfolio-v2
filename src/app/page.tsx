4988f4706cf39bda6e60ca83ef405338f74aa403
Samuel Mejia <smejia@earnest.com>
2023-06-20 10:23:57 -0500

    remove canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,11 @@ async function Home() {
 
   return (
     <>
-      <AnimatedCanvas
+    {/*   <AnimatedCanvas
         name="w-full h-full absolute inset-0	z-0"
         background="transparent"
         play={true}
-      />
+      /> */}
       <div className="mx-auto min-h-screen max-w-screen-xl relative px-6 z-10 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <Header />
